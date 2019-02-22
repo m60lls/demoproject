5b53de5e972a401bc0bb8cebbaf46121008b98d9
m60lls <billiepollet@gmail.com>
2019-02-22 11:47:54 +0100

    second commit

---
+++ _BACK_/server.js
@@ -0,0 +1,9 @@
+const X = require('express');
+const APP = X();
+const PORT = 2626;
+
+APP.use('/', X.static(__dirname + '/_SITE_/'));
+
+APP.listen(PORT, () => {
+    console.log(`\r\nNODE ::: I started my back end server on port ${PORT}.\r\n`);
+});
